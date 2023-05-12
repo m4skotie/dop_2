df03dc7e7fb0f3a12666818d21b7436fa6a53251
m4skotie <www.aidynkapitonov@gmail.com>
2023-05-12 12:43:34 +0300

    syn

---
+++ main.py
@@ -0,0 +1,47 @@
+import random
+input_word = input('Введите слово: ')
+file_path = 'synonym.txt'
+synonyms_dict = dict()
+try:
+    with open(file_path, 'r', encoding='UTF-8') as f:
+        synonyms = f.read().split('\n')
+    for line in synonyms:
+        if line:
+            word = line.split('-')[0].strip().lower()
+            if word not in synonyms_dict:
+                synonyms_dict[word] = [x.strip() for x in line.split('-')[1].strip().split(';')]
+            else:
+                synonyms_dict[word].extend([x.strip() for x in line.split('-')[1].strip().split(';') if x])
+
+    if input_word.lower() in synonyms_dict:
+        while True:
+            return_synonym = random.choice(synonyms_dict[input_word.lower()])
+            if return_synonym:
+                print(return_synonym)
+                break
+        while True:
+            synonym_correctness = input('Синоним устраивает?\n'
+                                        '1. Да\n'
+                                        '2. Нет\n')
+            if synonym_correctness == '1':
+                break
+            elif synonym_correctness == '2':
+                new_synonym = input('Введите новый синоним: ')
+                if new_synonym not in synonyms_dict[input_word.lower()]:
+                    synonyms_dict[input_word.lower()].append(new_synonym)
+                    string_to_write = ''
+                    for key in synonyms_dict:
+                        synonyms_dict[key] = list(filter(None, synonyms_dict[key]))
+                    for word, synonym in synonyms_dict.items():
+                        string_to_write += f'{word} - {"; ".join(synonym)}\n'
+                    with open(file_path, 'w', encoding='UTF-8') as f:
+                        f.write(string_to_write)
+                else:
+                    print('\033[31mСлово уже имеется в списке синонимов.\033[0m')
+                break
+            else:  # Если ввели не 1 или 2
+                print('\033[31mМожно ввести только 1 или 2.\033[0m')
+    else:  # Если слово отсутствует
+        print('\033[31mСлово отсутствует в файле.\033[0m')
+except FileNotFoundError:
+    print(f'\033[31mФайл {file_path} не найден.\033[0m')
